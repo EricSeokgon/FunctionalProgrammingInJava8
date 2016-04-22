ef68ade3a338b5f67a190c52d959d69e7b3425b9
hadesyi <hadesleeq@gmail.com>
2016-04-23 01:11:21 +0900

    Transforming a List

---
+++ src/chapter2/Transform.java
@@ -0,0 +1,37 @@
+package chapter2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Project: FunctionalProgrammingInJava8
+ * FileName: Transform
+ * Date: 2016-04-23
+ * Time: 오전 1:03
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Transform {
+    public static void main(String[] args) {
+        final List<String> friends = Arrays.asList("SeokGon", "JuYeong", "DaYe", "SeungJun");
+        final List<String> uppercaseNames = new ArrayList<String>();
+        final List<String> uppercaseNames2 = new ArrayList<String>();
+        for (String name : friends) {
+            uppercaseNames.add(name.toUpperCase());
+        }
+        System.out.println(uppercaseNames);
+
+        friends.forEach(name->uppercaseNames2.add(name.toUpperCase()));
+        System.out.println(uppercaseNames2);
+
+        friends.stream()
+                .map(name->name.toUpperCase())
+                .forEach(name-> System.out.println(name+ " "));
+
+    }
+
+}
+
+
