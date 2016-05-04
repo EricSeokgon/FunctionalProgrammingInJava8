bdb1e72e6368bda0707df71fff63d51604373155
hadesyi <hadesleeq@gmail.com>
2016-05-05 00:27:05 +0900

    Picking an Element

--- src/chapter2/PickAnElement.java
+++ src/chapter2/PickAnElement.java
@@ -7,14 +7,19 @@ import java.util.List;
  */
 public class PickAnElement {
     public static void pickName(
-            final List<String> names, final String stringLetter) {
+            final List<String> names, final String startingLetter) {
         String foundName = null;
         for (String name : names) {
-            if (name.startsWith((stringLetter))) {
+            if (name.startsWith(startingLetter)) {
                 foundName = name;
                 break;
             }
         }
-
+        System.out.print(String.format("A name starting with %s: ", startingLetter));
+        if (foundName != null) {
+            System.out.println(foundName);
+        } else {
+            System.out.println("No name found");
+        }
     }
 }
