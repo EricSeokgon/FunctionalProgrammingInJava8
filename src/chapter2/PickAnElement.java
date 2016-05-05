faea8b538b1230802b7dba546fb7253a62b05dbe
hadesyi <hadesleeq@gmail.com>
2016-05-06 06:01:00 +0900

    Picking an Element

--- src/chapter2/PickAnElement.java
+++ src/chapter2/PickAnElement.java
@@ -1,6 +1,7 @@
 package chapter2;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by hadeslee on 2016-05-04.
@@ -21,5 +22,15 @@ public class PickAnElement {
         } else {
             System.out.println("No name found");
         }
+    }
+
+    public static void pickName2(
+            final List<String> names, final String startingLetter) {
+        final Optional<String> foundName =
+                names.stream()
+                        .filter(name ->name.startsWith(startingLetter))
+                        .findFirst();
+        System.out.println(String.format("A name starting with %s: %s",
+                startingLetter, foundName.orElse("No name found")));
     }
 }
