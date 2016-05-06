88c44e530d222cb343d06e0e286d914256f9f54c
hadesyi <hadesleeq@gmail.com>
2016-05-07 00:39:18 +0900

    Picking an Element

---
+++ src/chapter2/PickAnElementElegant.java
@@ -0,0 +1,25 @@
+package chapter2;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Project: FunctionalProgrammingInJava8
+ * FileName: PickAnElementElegant
+ * Date: 2016-05-07
+ * Time: 오전 12:38
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class PickAnElementElegant {
+    public static void pickName(
+            final List<String> names, final String startingLetter) {
+        final Optional<String> foundName =
+                names.stream()
+                        .filter(name ->name.startsWith(startingLetter))
+                        .findFirst();
+        System.out.println(String.format("A name starting with %s: %s",
+                startingLetter, foundName.orElse("No name found")));
+    }
+}
