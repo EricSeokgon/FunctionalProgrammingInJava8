3681240045c0032ac48d26dfe79b3ed17587dcda
hadesyi <hadesleeq@gmail.com>
2016-05-09 13:44:15 +0900

    Reducing a Collection to a Single Value

---
+++ src/chapter2/PickALongest.java
@@ -0,0 +1,15 @@
+package chapter2;
+
+import static chapter2.Folks.friends;
+
+/**
+ * Created by hadeslee on 2016-05-09.
+ */
+public class PickALongest {
+    public static void main(String[] args) {
+        System.out.println("Total number of characters in all names: " +
+                friends.stream()
+                        .mapToInt(name -> name.length())
+                        .sum());
+    }
+}
