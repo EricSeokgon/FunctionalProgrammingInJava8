7868c57e8e9fe397ac07080856d13f44efddf84b
hadesyi <hadesleeq@gmail.com>
2016-05-13 00:11:47 +0900

    Iterating a String

--- src/chapter3/IterateString.java
+++ src/chapter3/IterateString.java
@@ -10,10 +10,16 @@ package chapter3;
  * To change this template use File | Settings | File Templates.
  */
 public class IterateString {
-
+    private static void printChar(int aChar) {
+        System.out.println((char)(aChar));
+    }
     public static void main(String[] args) {
         final String str = "w00t";
         str.chars().forEach(ch -> System.out.println(ch));
         str.chars().forEach(System.out::println);
+        str.chars().forEach(IterateString::printChar);
+        str.chars()
+                .mapToObj(ch -> Character.valueOf((char)ch))
+                .forEach(System.out::println);
     }
 }
