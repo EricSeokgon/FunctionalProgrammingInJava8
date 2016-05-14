622e85720fc5418c0f0e209577186140c624cd9d
hadesyi <hadesleeq@gmail.com>
2016-05-15 02:36:55 +0900

    Sorting with a Comparator

---
+++ src/chapter3/Person.java
@@ -0,0 +1,20 @@
+package chapter3;
+
+/**
+ * Project: FunctionalProgrammingInJava8
+ * FileName: Person
+ * Date: 2016-05-14
+ * Time: 오전 12:10
+ * Author: redfo
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Person {
+    final private String name;
+    final private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
