3155b0b81ca561ee68374a0a3fa6220fd866b324
hadesyi <hadesleeq@gmail.com>
2016-05-18 00:05:04 +0900

    Sorting with a Comparator

--- src/chapter3/Compare.java
+++ src/chapter3/Compare.java
@@ -2,6 +2,7 @@ package chapter3;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Project: FunctionalProgrammingInJava8
@@ -13,11 +14,25 @@ import java.util.List;
  * To change this template use File | Settings | File Templates.
  */
 public class Compare {
+
+    public static void printPeople(
+            final String message, final List<Person> people) {
+        System.out.println(message);
+        people.forEach(System.out::println);
+    };
+
     public static void main(String[] args) {
+
         final List<Person> people = Arrays.asList(
                 new Person("John", 20),
                 new Person("Sara", 21),
                 new Person("Jane", 21),
                 new Person("Greg", 35));
+
+        List<Person> ascendingAge =
+                people.stream()
+                .sorted((person1, person2)->person1.ageDifference(person2))
+                .collect(Collectors.toList());
+        printPeople("sorted in ascending order of age : ", ascendingAge);
     }
 }
