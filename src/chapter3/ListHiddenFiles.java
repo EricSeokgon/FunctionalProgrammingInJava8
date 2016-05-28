86ed52fa67ec592569c9e983ee84e049fda7ce50
hadesyi <hadesleeq@gmail.com>
2016-05-29 00:35:38 +0900

    Listing Select Files in a Directory

--- src/chapter3/ListHiddenFiles.java
+++ src/chapter3/ListHiddenFiles.java
@@ -1,6 +1,7 @@
 package chapter3;
 
 import java.io.File;
+import java.util.Arrays;
 
 /**
  * Project: FunctionalProgrammingInJava8
@@ -12,5 +13,11 @@ import java.io.File;
  * To change this template use File | Settings | File Templates.
  */
 public class ListHiddenFiles {
-    final File[] files = new File(".").listFiles(file -> file.isHidden());
+    public static void main(String[] args) {
+        final File[] files = new File(".").listFiles(file -> file.isHidden());
+
+        new File(".").listFiles(File::isHidden);
+
+        Arrays.stream(files).forEach(System.out::println);
+    }
 }
