cb405d1b978ea4f4443969c1c177bc5698049dcc
hadesyi <hadesleeq@gmail.com>
2016-06-10 00:14:11 +0900

    Making the API Intuitive and Flue

---
+++ src/chapter4/FluentMailer.java
@@ -0,0 +1,31 @@
+package chapter4;
+
+/**
+ * Created by redfo on 2016-06-10.
+ */
+public class FluentMailer {
+    private FluentMailer() {}
+
+    public static void send(final Consumer<FluentMailer> block) {
+        final FluentMailer mailer = new FluentMailer();
+        block.accept(mailer);
+        System.out.println("sending...");
+    }
+
+    //...
+    public static void main(final String[] args) {
+        FluentMailer.send(mailer ->
+                mailer.from("build@agiledeveloper.com")
+                        .to("venkats@agiledeveloper.com")
+                        .subject("build notification")
+                        .body("...much better..."));
+    }
+
+    public FluentMailer from(final String address) { /*... */; return this; }
+
+    public FluentMailer to(final String address)   { /*... */; return this; }
+
+    public FluentMailer subject(final String line) { /*... */; return this; }
+
+    public FluentMailer body(final String message) { /*... */; return this; }
+}
