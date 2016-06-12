3cbef0ffe69e4f6044ae609ca86d5d6356b332c1
이석곤 <leesk55@kesti.co.kr>
2016-06-13 08:42:34 +0900

    Using ARM

---
+++ src/chapter5/FileWriterARM.java
@@ -0,0 +1,34 @@
+package chapter5;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Created by hadeslee on 2016-06-13.
+ */
+public class FileWriterARM implements AutoCloseable {
+    private final FileWriter writer;
+
+    public FileWriterARM(final String fileName) throws IOException {
+        writer = new FileWriter(fileName);
+    }
+
+    public void writeStuff(final String message) throws IOException {
+        writer.write(message);
+    }
+
+    public void close() throws IOException {
+        System.out.println("close called automatically...");
+        writer.close();
+    }
+
+    //...
+
+    public static void main(final String[] args) throws IOException {
+        try(final FileWriterARM writerARM = new FileWriterARM("peekaboo.txt")) {
+            writerARM.writeStuff("peek-a-boo");
+
+            System.out.println("done with the resource...");
+        }
+    }
+}
