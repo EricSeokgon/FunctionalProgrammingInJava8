6e79c7eb6030752e7b9894e01f3f8bf320323191
hadesyi <hadesleeq@gmail.com>
2016-06-16 00:02:46 +0900

    Using Higher-Order Functions

--- src/chapter5/FileWriterEAM.java
+++ src/chapter5/FileWriterEAM.java
@@ -1,5 +1,7 @@
 package chapter5;
 
+import chapter4.UseInstance;
+
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -8,15 +10,27 @@ import java.io.IOException;
  */
 public class FileWriterEAM {
     private final FileWriter writer;
+
     private FileWriterEAM(final String fileName) throws IOException {
         writer = new FileWriter(fileName);
     }
+
     private void close() throws IOException {
         System.out.println("close called automatically...");
         writer.close();
     }
+
     public void writeStuff(final String message) throws IOException {
         writer.write(message);
     }
-//...
+
+    public static void use(final String fileName,
+                           final UseInstance<FileWriterEAM, IOException> block) throws IOException {
+        final FileWriterEAM writerEAM = new FileWriterEAM(fileName);
+        try {
+            block.accept(writerEAM);
+        } finally {
+            writerEAM.close();
+        }
+    }
 }
