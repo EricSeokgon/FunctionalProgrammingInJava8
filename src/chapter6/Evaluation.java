041e3350198f2e02ab60db45d7fd7309b5086cb5
hadesyi <hadesleeq@gmail.com>
2016-06-23 09:31:51 +0900

    Starting with Eager Evaluation

--- src/chapter6/Evaluation.java
+++ src/chapter6/Evaluation.java
@@ -9,5 +9,10 @@ public class Evaluation {
         simulateTimeConsumingOp(2000);
         return value > 100;
     }
-//...
+
+    public static void eagerEvaluator(
+            final boolean input1, final boolean input2) {
+        System.out.println("eagerEvaluator called...");
+        System.out.println("accept?: " + (input1 && input2));
+    }
 }
