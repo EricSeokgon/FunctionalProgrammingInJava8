4206c1a81c90c871ae4a440c192ad26c6bfc58e3
hadesyi <hadesleeq@gmail.com>
2016-06-28 08:31:43 +0900

    Designing for Lazy Evaluation

--- src/chapter6/Evaluation.java
+++ src/chapter6/Evaluation.java
@@ -11,6 +11,13 @@ public class Evaluation {
         simulateTimeConsumingOp(2000);
         return value > 100;
     }
+    //...
+
+    public static void simulateTimeConsumingOp(final int millseconds) {
+        try {
+            Thread.sleep(2000);
+        } catch(Exception ex) { throw new RuntimeException(ex); }
+    }
 
     public static void eagerEvaluator(
             final boolean input1, final boolean input2) {
@@ -22,5 +29,16 @@ public class Evaluation {
             final Supplier<Boolean> input1, final Supplier<Boolean> input2) {
         System.out.println("lazyEvaluator called...");
         System.out.println("accept?: " + (input1.get() && input2.get()));
+    }
+
+    public static void main(final String[] args) {
+
+        System.out.println("//" + "START:EAGER_OUTPUT");
+        eagerEvaluator(evaluate(1), evaluate(2));
+        System.out.println("//" + "END:EAGER_OUTPUT");
+
+        System.out.println("//" + "START:LAZY_OUTPUT");
+        lazyEvaluator(() -> evaluate(1), () -> evaluate(2));
+        System.out.println("//" + "END:LAZY_OUTPUT");
     }
 }
