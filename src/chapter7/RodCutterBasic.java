0807cc86d500b33e2c300760999cc60a61e34e48
hadesyi <hadesleeq@gmail.com>
2016-07-03 00:13:30 +0900

    Speeding Up with Memoization

---
+++ src/chapter7/RodCutterBasic.java
@@ -0,0 +1,14 @@
+package chapter7;
+
+import java.util.List;
+
+/**
+ * Created by redfo on 2016-07-03.
+ */
+public class RodCutterBasic {
+    final List<Integer> prices;
+    public RodCutterBasic(final List<Integer> pricesForLength) {
+        prices = pricesForLength;
+    }
+//...
+}
