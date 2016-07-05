7d561994e6bbe400ea287c4978cc53e5e1158af1
hadesyi <hadesleeq@gmail.com>
2016-07-06 08:39:45 +0900

    Plain-Vanilla Recursion

--- src/chapter7/RodCutterBasic.java
+++ src/chapter7/RodCutterBasic.java
@@ -8,11 +8,22 @@ import java.util.List;
  */
 public class RodCutterBasic {
     final List<Integer> prices;
+
     public RodCutterBasic(final List<Integer> pricesForLength) {
         prices = pricesForLength;
     }
-//...
-final List<Integer> priceValues =
-        Arrays.asList(2, 1, 1, 2, 2, 2, 1, 8, 9, 15);
+
+    //...
+    final List<Integer> priceValues =
+            Arrays.asList(2, 1, 1, 2, 2, 2, 1, 8, 9, 15);
     final RodCutterBasic rodCutter = new RodCutterBasic(priceValues);
+
+    public int maxProfit(final int length) {
+        int profit = (length <= prices.size()) ? prices.get(length - 1) : 0;
+        for (int i = 1; i < length; i++) {
+            int priceWhenCut = maxProfit(i) + maxProfit(length - i);
+            if (profit < priceWhenCut) profit = priceWhenCut;
+        }
+        return profit;
+    }
 }
