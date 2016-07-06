be895bee02e6fbd20bcac91b96702bba3dbc06fa
hadesyi <hadesleeq@gmail.com>
2016-07-07 08:35:03 +0900

    Adding a Level of Indirection

--- src/chapter6/Holder.java
+++ src/chapter6/Holder.java
@@ -7,11 +7,34 @@ import java.util.function.Supplier;
  */
 public class Holder {
     private Supplier<Heavy> heavy = () -> createAndCacheHeavy();
+
     public Holder() {
         System.out.println("Holder created");
     }
+
     public Heavy getHeavy() {
         return heavy.get();
     }
-//...
+    //...
+
+    private synchronized Heavy createAndCacheHeavy() {
+        class HeavyFactory implements Supplier<Heavy> {
+            private final Heavy heavyInstance = new Heavy();
+
+            public Heavy get() { return heavyInstance; }
+        }
+
+        if(!HeavyFactory.class.isInstance(heavy)) {
+            heavy = new HeavyFactory();
+        }
+
+        return heavy.get();
+    }
+
+    public static void main(final String[] args) {
+        final Holder holder = new Holder();
+        System.out.println("deferring heavy creation...");
+        System.out.println(holder.getHeavy());
+        System.out.println(holder.getHeavy());
+    }
 }
