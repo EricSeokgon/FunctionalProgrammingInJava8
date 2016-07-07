dbf901a2511f9aef276b1834d5809365f238c5c0
hadesyi <hadesleeq@gmail.com>
2016-07-08 00:42:00 +0900

    Using Function Composition

---
+++ src/chapter8/Tickers.java
@@ -0,0 +1,14 @@
+package chapter8;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by redfo on 2016-07-08.
+ */
+public class Tickers {
+    public static final List<String> symbols = Arrays.asList(
+            "AMD", "HPQ", "IBM", "TXN", "VMW", "XRX", "AAPL", "ADBE",
+            "AMZN", "CRAY", "CSCO", "DELL", "GOOG", "INTC", "INTU",
+            "MSFT", "ORCL", "TIBX", "VRSN", "YHOO");
+}
